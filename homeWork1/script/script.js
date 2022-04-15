d4c483979864fab101bffbd14d4760806cf3f577
Napolion4ik <dzibuaoleg20@gmail.com>
2022-04-15 16:22:48 +0300

    Написання коду програми

---
+++ homeWork1/script/script.js
@@ -0,0 +1,14 @@
+let users = prompt('Введите пользователей через ,');
+let arr = users.split(',')
+
+let listItem = document.querySelector('.list');
+let html = ``;
+
+
+arr.forEach(function (item, index) {
+    let usersName = item;
+    let number = ++index;
+    html += (`<li>Номер пользователя: <strong>${number}</strong>. Имя пользователя: <strong>${usersName}</strong></li>.`);
+    listItem.innerHTML = html;
+    
+})
